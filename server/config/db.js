f02f4ebbd21a7d1cf820267a7511c6087dedeb04
manish kumar <134628189+manishkumarth@users.noreply.github.com>
2026-04-26 13:15:10 +0530

    Enhance MongoDB connection logic with status check
    Add connection status check for MongoDB.

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,14 @@
 
 
 const mongoose=require('mongoose');
+let isConnected=false
 const connectToDb=async()=>{
     try{
+        if(isConnected){
+            return
+        }
       const res=await mongoose.connect(process.env.MONGODB_URL);
+        isConnected=true
       console.log("Connected to MongoDB",res.connection.host);
     }catch(err){
         console.log("Faild to connect DB",err)
