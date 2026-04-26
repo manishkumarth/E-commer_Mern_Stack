d2f83307135957d22eb9b55b0c8520b99b717aaa
manishkumar <manish12099@gmail.com>
2026-04-26 11:50:49 +0530

    multer lib path changes in router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,15 +1,16 @@
 const { Router } = require("express");
 const multer = require("multer");
-const fs = require("fs");
-const path = require("path");
 
-const uploadDir = path.join(__dirname, "../uploads");
+const storage = multer.memoryStorage();
 
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB limit
+  },
+});
 
-const upload = multer({ dest: uploadDir });
+module.exports = upload;
 
 const { registerUser, loginUser, getProfile, sendOtp, verifyOtp, forgetPassword, deleteUser, getAllUsers } = require("../controller/user");
 const { addProduct, deleteProduct, getSellerProducts, getAllProducts, updateProduct, getProductById, searchByImage, generateEmbeddingSingle } = require("../controller/product")
